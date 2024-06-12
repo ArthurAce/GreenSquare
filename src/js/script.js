661caac1bd0e6637f8af15d059e1af68c21c6257
Arthur <forceaceflash@gmail.com>
2024-06-12 18:58:12 +0300

    init index css and js files

---
+++ src/js/script.js
@@ -0,0 +1,55 @@
+const slides = document.querySelectorAll('.slides img');
+const slidesTitle = document.querySelectorAll('.slider__title');
+let slideIndex  = 0;
+let intervalId = null;
+let slideTitleIndex = 0;
+
+
+
+document.addEventListener("DOMContentLoaded", initializeSlider); 
+
+function initializeSlider(){
+
+    if(slides.length > 0){
+        slides[slideIndex].classList.add("displaySlide")
+        intervalId = setInterval(nextSlide, 5500)
+    }
+
+}
+
+function showSlide(index){
+
+    if(index >= slides.length){
+        slideIndex = 0
+    }
+    else if(index < 0){
+        slideIndex = slides.length - 1
+    }
+
+    if(index >= slidesTitle.length){
+        slideTitleIndex = 0
+    }
+    else if(index < 0){
+        slideTitleIndex = slidesTitle.length - 1
+    }
+
+
+    slides.forEach(slide => {
+        slide.classList.remove("displaySlide")
+    });
+    slides[slideIndex].classList.add("displaySlide")
+
+}
+
+function prevSlide(){
+    clearInterval(intervalId)
+        slideIndex--
+        showSlide(slideIndex)
+
+}
+
+function nextSlide(){
+    slideIndex++
+    showSlide(slideIndex)
+
+}
