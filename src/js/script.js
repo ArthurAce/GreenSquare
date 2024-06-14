12971f9a4b68882d1f9381ac9e94714854499953
Arthur <forceaceflash@gmail.com>
2024-06-14 17:35:02 +0300

    add fav page, finish bc page

--- src/js/script.js
+++ src/js/script.js
@@ -54,4 +54,62 @@ function nextSlide(){
 
 }
 
+///
+
+const slides2 = document.querySelectorAll('.slides2 img');
+const slidesTitle2 = document.querySelectorAll('.slider__title2');
+let slideIndex2  = 0;
+let intervalId2 = null;
+let slideTitleIndex2 = 0;
+
+
+
+document.addEventListener("DOMContentLoaded", initializeSlider2); 
+
+function initializeSlider2(){
+
+    if(slides2.length > 0){
+        slides2[slideIndex2].classList.add("displaySlide2")
+        intervalId2 = setInterval(nextSlide2, 5400)
+    }
+
+}
+
+function showSlide2(index){
+
+    if(index >= slides2.length){
+        slideIndex2 = 0
+    }
+    else if(index < 0){
+        slideIndex2 = slides2.length - 1
+    }
+
+    if(index >= slidesTitle2.length){
+        slideTitleIndex2 = 0
+    }
+    else if(index < 0){
+        slideTitleIndex2 = slidesTitle2.length - 1
+    }
+
+
+    slides2.forEach(slide2 => {
+        slide2.classList.remove("displaySlide2")
+    });
+    slides2[slideIndex2].classList.add("displaySlide2")
+
+}
+
+function prevSlide2(){
+    clearInterval(intervalId2)
+        slideIndex2--
+        showSlide2(slideIndex2)
+
+}
+
+function nextSlide2(){
+    slideIndex2++
+    showSlide2(slideIndex2)
+
+}
+
 
